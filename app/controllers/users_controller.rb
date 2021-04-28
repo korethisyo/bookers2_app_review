1baf7c2df0886dc66732c7fae80eb4f4c8c67401
korethisyo <korethis0816@gmail.com>
2021-04-28 11:05:18 +0000

    [Update]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save(book_params)
+    redirect_to books_path
   end
 
   def show
-    @user = User.find(book_params[:id])
+    @user = User.find(params[:id])
+    @book = @user.books
+    @new_book = Book.new
   end
 
   def edit
