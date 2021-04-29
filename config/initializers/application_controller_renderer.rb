92bb07c1374c5580471809747f7750589fc32ddd
korethisyo <korethis0816@gmail.com>
2021-04-29 04:48:08 +0000

    [Update]bookers2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a7125bbe1774b7ccfb8d666d8e2cd694b643530f6a61fdfd4e82ba2ec39d6d6b86bc6460fcee95d3c396eb8b80939d8fe3a4be0a80deb1252487c1c1bc35b730'
