0b524ca04478f4a0a83598191b8d2cad411ccfc8
korethisyo <korethis0816@gmail.com>
2021-05-06 12:10:12 +0000

    [Update]bookers2

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,11 @@
 class SearchController < ApplicationController
   before_action :authenticate_user!
-  
+
   def search
     @word = params[:word]
     @model = params[:model]
     @method = params[:method]
-    if @model == "User"
+    if @model == "user"
       @users = User.search_for(@word, @method)
     else
       @books = Book.search_for(@word, @method)
